93e606c150dd4adb2034f9a23d9e845edcf2d967
CarsonBills <CarsonMBills@Gmail.com>
2015-09-13 16:31:57 -0400

    app set up

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
-	res.render("pages/index.html")
+	res.render("pages/index")
 })
+
+app.listen(8080);
+console.log('port 8080')
