31e8109f9d93d4d113523dbc6ab9330e3863eee9
CarsonBills <CarsonMBills@Gmail.com>
2015-09-22 09:52:46 -0400

    all titles and covers render on library page

--- config/library.js
+++ config/library.js
@@ -8,7 +8,132 @@ exports.getLibrary = function(){
 		{
 			title: "Broken Play",
 			cover: "broken_play.jpg"
-		}
+		},
+
+		{
+			title: "A Lady in Waiting",
+			cover: "a_lady_in_waiting.jpg"
+		},
+		{
+			title: "Aged To Perfection",
+			cover: "aged_to_perfection.jpg"
+		},
+		{
+			title: "Arms of Love",
+			cover: "arms_of_love.jpg"
+		},
+		{
+			title: "At Love's Command",
+			cover: "at_love's_command.jpg"
+		},
+		{
+			title: "Brodie",
+			cover: "brodie.jpg"
+		},
+		{
+			title: "Cherry Pie",
+			cover: "cherry_pie.jpg"
+		},
+		{
+			title: "conall",
+			cover: "conall.png"
+		},
+		{
+			title: "Defeated By Love",
+			cover: "defeated_by_love.jpg"
+		},
+		{
+			title: "Devil in my Arms",
+			cover: "devil_in_my_arms.jpg"
+		},
+		{
+			title: "Douglas",
+			cover: "douglas.jpg"
+		},
+		{
+			title: "Esprit of Corps",
+			cover: "esprit_of_corps.jpg"
+		},
+		{
+			title: "Fight For Love",
+			cover: "fight_for_love.jpg"
+		},
+		{
+			title: "Gift of Desire",
+			cover: "gift_of_desire.jpg"
+		},
+		{
+			title: "Hamish",
+			cover: "hamish.jpg"
+		},
+		{
+			title: "Islands",
+			cover: "islands.jpg"
+		},
+		{
+			title: "Jewels of the Nile",
+			cover: "jewels_of_the_nile.jpg"
+		},
+		{
+			title: "Love and War",
+			cover: "love_and_war.jpg"
+		},
+		{
+			title: "Love Betrayed",
+			cover: "love_betrayed.jpg"
+		},
+		{
+			title: "Love in Exile",
+			cover: "love_in_exile.jpg"
+		},
+		{
+			title: "Love Returns",
+			cover: "love_returns.jpg"
+		},
+		{
+			title: "Love Under Siege",
+			cover: "love_under_siege.jpg"
+		},
+		{
+			title: "Love's Fortress",
+			cover: "love's_fortress.jpg"
+		},
+		{
+			title: "Love's Strategy",
+			cover: "love's_strategy.jpg"
+		},
+		{
+			title: "Love's Surrender",
+			cover: "love's_surrender.jpg"
+		},
+		{
+			title: "Play it Again, Sam",
+			cover: "play_it_again_sam.jpg"
+		},
+		{
+			title: "Prisoner of Love",
+			cover: "prisoner_of_love.jpg"
+		},
+		{
+			title: "Retreat From Love",
+			cover: "retreat_from_love.jpg"
+		},
+		{
+			title: "Tempt the Cougar",
+			cover: "tempt_the_cougar.jpg"
+		},
+		{
+			title: "Tempting A Devil",
+			cover: "tempting_a_devil.jpg"
+		},
+		{
+			title: "The Courage to Love",
+			cover: "the_courage_to_love.jpg"
+		},
+		{
+			title: "The Devil's Thief",
+			cover: "the_devil's_thief.jpg"
+		},
 	]
 	return library
 }
