7b68802b21bbbf42d97998eb3e255e7fccd1288b
CarsonBills <CarsonMBills@Gmail.com>
2015-09-23 11:19:02 -0400

    library divided into series

--- config/library.js
+++ config/library.js
@@ -1,22 +1,9 @@
 exports.getLibrary = function(){
 
-	var library = [
-		{
-			title: "Calling the Play",
-			cover: "calling_the_play.jpg"
-		},
-		{
-			title: "Broken Play",
-			cover: "broken_play.jpg"
-		},
-		{
-			title: "A Lady in Waiting",
-			cover: "a_lady_in_waiting.jpg"
-		},
-		{
-			title: "Aged To Perfection",
-			cover: "aged_to_perfection.jpg"
-		},
+	var library = {
+
+	"Brothers in Arms": [
+
 		{
 			title: "Arms of Love",
 			cover: "arms_of_love.jpg"
@@ -26,52 +13,12 @@ exports.getLibrary = function(){
 			cover: "at_love's_command.jpg"
 		},
 		{
-			title: "Brodie",
-			cover: "brodie.jpg"
-		},
-		{
-			title: "Cherry Pie",
-			cover: "cherry_pie.jpg"
-		},
-		{
-			title: "Conall",
-			cover: "conall.png"
-		},
-		{
 			title: "Defeated By Love",
 			cover: "defeated_by_love.jpg"
 		},
 		{
-			title: "Devil in My Arms",
-			cover: "devil_in_my_arms.jpg"
-		},
-		{
-			title: "Douglas",
-			cover: "douglas.jpg"
-		},
-		{
-			title: "Esprit of Corps",
-			cover: "esprit_of_corps.jpg"
-		},
-		{
 			title: "Fight For Love",
 			cover: "fight_for_love.jpg"
-		},
-		{
-			title: "Gift of Desire",
-			cover: "gift_of_desire.jpg"
-		},
-		{
-			title: "Hamish",
-			cover: "hamish.jpg"
-		},
-		{
-			title: "Islands",
-			cover: "islands.jpg"
-		},
-		{
-			title: "Jewels of the Nile",
-			cover: "jewels_of_the_nile.jpg"
 		},
 		{
 			title: "Love and War",
@@ -105,10 +52,7 @@ exports.getLibrary = function(){
 			title: "Love's Surrender",
 			cover: "love's_surrender.jpg"
 		},
-		{
-			title: "Play it Again, Sam",
-			cover: "play_it_again_sam.jpg"
-		},
+
 		{
 			title: "Prisoner of Love",
 			cover: "prisoner_of_love.jpg"
@@ -117,22 +61,113 @@ exports.getLibrary = function(){
 			title: "Retreat From Love",
 			cover: "retreat_from_love.jpg"
 		},
+
 		{
-			title: "Tempt The Cougar",
-			cover: "tempt_the_cougar.jpg"
+			title: "The Courage to Love",
+			cover: "the_courage_to_love.jpg"
+		},
+
+	],
+
+	"The Saint's Devils": [
+
+		{
+			title: "Devil in My Arms",
+			cover: "devil_in_my_arms.jpg"
 		},
 		{
 			title: "Tempting a Devil",
 			cover: "tempting_a_devil.jpg"
 		},
 		{
-			title: "The Courage to Love",
-			cover: "the_courage_to_love.jpg"
-		},
-		{
 			title: "The Devil's Thief",
 			cover: "the_devil's_thief.jpg"
 		},
-	]
+
+	],
+
+	"Mercury Rising" : [
+
+		{
+			title: "Cherry Pie",
+			cover: "cherry_pie.jpg"
+		},
+
+	],
+
+	"93rd Highlanders" : [
+
+		{
+			title: "Brodie",
+			cover: "brodie.jpg"
+		},
+		{
+			title: "Conall",
+			cover: "conall.png"
+		},
+		{
+			title: "Douglas",
+			cover: "douglas.jpg"
+		},
+		{
+			title: "Hamish",
+			cover: "hamish.jpg"
+		},
+
+	],
+
+	"Birmingham Rebels" : [
+
+		{
+			title: "Calling the Play",
+			cover: "calling_the_play.jpg"
+		},
+		{
+			title: "Broken Play",
+			cover: "broken_play.jpg"
+		},
+
+	],
+
+	"Standalone Romance" : [
+
+		{
+			title: "A Lady in Waiting",
+			cover: "a_lady_in_waiting.jpg"
+		},
+		{
+			title: "Islands",
+			cover: "islands.jpg"
+		},
+		{
+			title: "Play it Again, Sam",
+			cover: "play_it_again_sam.jpg"
+		},
+
+	],
+
+	"Romance Anthologies" : [
+
+		{
+			title: "Aged To Perfection",
+			cover: "aged_to_perfection.jpg"
+		},
+		{
+			title: "Esprit of Corps",
+			cover: "esprit_of_corps.jpg"
+		},
+		{
+			title: "Gift of Desire",
+			cover: "gift_of_desire.jpg"
+		},
+		{
+			title: "Jewels of the Nile",
+			cover: "jewels_of_the_nile.jpg"
+		},
+		{
+			title: "Tempt The Cougar",
+			cover: "tempt_the_cougar.jpg"
+		},
+	]}
 	return library
 }
